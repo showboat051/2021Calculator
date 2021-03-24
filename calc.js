4ec891f0b5bcec94677d8164b9ae76c82e5d21b8
showboat051 <rashad.davis87@yahoo.com>
2021-03-23 19:10:31 -0500

    added numbered buttons

---
+++ calc.js
@@ -0,0 +1,43 @@
+                                //// Button Resources ////////
+
+const num1Btn = document.getElementById('num1').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 1;
+});
+
+const num2Btn = document.getElementById('num2').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 2;
+});
+
+const num3Btn = document.getElementById('num3').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 3;
+});
+
+const num4Btn = document.getElementById('num4').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 4;
+});
+
+const num5Btn = document.getElementById('num5').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 5;
+});
+
+const num6Btn = document.getElementById('num6').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 6;
+});
+
+const num7Btn = document.getElementById('num7').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 7;
+});
+
+const num8Btn = document.getElementById('num8').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 8;
+});
+
+const num9Btn = document.getElementById('num9').addEventListener("click", function() {
+    document.getElementById("display").innerHTML = 9;
+});
+
+
+
+
+
+
