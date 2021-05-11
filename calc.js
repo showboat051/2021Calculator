d636f6272086841978b5a6614845ea0fcf848d8e
showboat051 <rashad.davis87@yahoo.com>
2021-05-10 23:03:31 -0500

    needs styling

--- calc.js
+++ calc.js
@@ -11,19 +11,6 @@ let result;
 let operator = ["+", "-", "*", "/"];
 
 
-//**********************BUTTON CLICKS ON THE CALCULATOR************************************** */
-// calculatorButtons.addEventListener("click", function (e) {
-//   num1 = valueDisplay.innerHTML += e.target.value;
-// //   num2 = valueDisplay.innerHTML += e.target.value;
-//     if (e.target.value == "+" || e.target.value == "-" || e.target.value == "*" ||  e.target.value == "/" ) {  
-//     console.log("clicked an operator", e.target.value);
-//     // num1.split(",").push(numToCalc);
-//     }
-//     else {
-//         console.log("clicked a number", e.target.value);
-//     }
-// });
-
 //Button clicks for numbers 
 for (var i = 0; i < Buttons.length; i++) {
     Buttons[i].addEventListener("click" , function(e) {
@@ -50,37 +37,3 @@ function solve () {
     let result = eval(input);
     valueDisplay.innerText = result;
 } // end of solve()
-
-function calculate(num) {
-    switch(operator) {
-        case "+":
-            result = prevNum + currentNumber;
-            break;
-         case "-":
-            result = prevNum - currentNumber;
-            break;
-         case "*":
-             result = prevNum * currentNumber;
-             break;
-         case "/":
-             result = prevNum / currentNumber;
-             break;    
-    }
-     valueDisplay.innerHTML = result;
- }// end of calculate()
-
-
-
-//**********************DISPLAY NUMBERS ON SCREEN************************************** */
-// valueDisplay.calculatorButtons.addEventListener()
-
-
-//**********************FUNCTIONS************************************** */
-
-
-
-function erase() {
-    console.log("clicked erase");
-    valueDisplay.innerHTML = "";
-}
-
